a2cabd58802d50cbe07b466aab8f71bdedc33e7a
Justin Lim <liminator1106@gmail.com>
2016-06-23 09:42:52 -0700

    fixed reducer HMR

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 
-import reducer from './reducers';
+import reducer from './reducers/index';
 
-export default function configureStore() {
-  const store = applyMiddleware(promise)(createStore);
+export default function configureStore(initialState) {
+  const store = createStore(
+    reducer,
+    initialState,
+    compose(
+      applyMiddleware(
+        promise
+      )
+    )
+  );
 
   if(module.hot) {
-    module.hot.accept('./reducers', () => {
-      const nextReducer = require('./reducers');
+    module.hot.accept(() => {
+      const nextReducer = require('./reducers/index.js');
       store.replaceReducer(nextReducer);
     })
   }
--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 module.exports = function (app) {
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/../index.html'));
-  });
-
   app.post('/test', (req, res) => {
     console.log('hello');
     res.send('hello');
   });
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../index.html'));
+  });
 };
