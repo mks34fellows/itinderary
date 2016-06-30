96bbe8d96d3292785394055b30049ae9988888eb
Ramin <ramin_yavari03@yahoo.com>
2016-06-30 15:16:07 -0700

    Feature: Implemeneted basic form validation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,6 +5,8 @@ export const INPUT_SUBMITTED = 'INPUT_SUBMITTED';
 export function submitInput(input) {
   console.log('HERE IS THE INPUT', input)
 
+  
+
   const request = axios.post('/yelp', input);
 
   return {
--- client/src/containers/user_input.js
+++ client/src/containers/user_input.js
@@ -19,24 +19,38 @@ class UserInput extends Component {
   render() {
     const { fields: {feeling}, handleSubmit, onChange } = this.props;
 
+    console.log('this is feelings validation',feeling)
+
     return(
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-      
-          <input type="radio" name='feeling' value='adventurous' onChange={feeling.onChange} checked={feeling.value === 'adventurous'} /> Adventurous <br>
-          <input type="radio" name='feeling' value='classy'  onChange={feeling.onChange} checked={feeling.value === 'classy'} /> Classy <br>
+        <div>
+          <input type="radio" name='feeling' value='adventurous' onChange={feeling.onChange} checked={feeling.value === 'adventurous'} /> Adventurous
+          <input type="radio" name='feeling' value='classy'  onChange={feeling.onChange} checked={feeling.value === 'classy'} /> Classy
           <input type="radio" name='feeling' value='competitive' onChange={feeling.onChange} checked={feeling.value === 'competitive'} /> Competitive
           <input type="radio" name='feeling' value='creative' onChange={feeling.onChange} checked={feeling.value === 'creative'}/> Creative
           <input type="radio" name='feeling' value='lazy' onChange={feeling.onChange} checked={feeling.value === 'lazy'} /> Lazy
           <input type="radio" name='feeling' value='ratchet' onChange={feeling.onChange} checked={feeling.value === 'ratchet'} /> Ratchet
-       
-
-        <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
+                 {feeling.touched ? feeling.error : ''}
+        </div>
       </form>
-    )
+    );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if(!values.feeling) {
+    errors.feeling = 'Please select something';
+  }
+  return errors;
 }
 
 export default reduxForm({
   form: 'UserInput',
-  fields: ['feeling']
+  fields: ['feeling'],
+  validate
 }, null, { submitInput })(UserInput);
+
+
