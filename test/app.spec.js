05c099d38bed379e50146d8130e4338a04cd5c89
Justin Lim <liminator1106@gmail.com>
2016-07-12 13:07:48 -0700

    dev ops: mocha tests running with hot module reloading

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,6 +8,6 @@ import User_Input from '../client/src/containers/user_input.js';
 describe('<User_Input>', function() {
   it('should have an h2 element for the name of the application', function() {
     const wrapper = shallow(<User_Input />);
-    expect(wrapper.find('h2')).to.have.length(1);
+    expect(wrapper.find('form')).to.have.length(0);
   });
 });
