aed8504c5347145f6d2824a863f04fc3b711698c
Justin Lim <liminator1106@gmail.com>
2016-07-20 10:21:39 -0700

    removed res.sendFile

--- server/routes.js
+++ server/routes.js
@@ -18,7 +18,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve('/index.html'));
+  // });
 };
