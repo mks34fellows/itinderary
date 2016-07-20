864b7354d1d68ab13a3cc1bee953bb7b1e7921b4
Justin Lim <liminator1106@gmail.com>
2016-07-20 10:42:18 -0700

    added webpack.prod.config file

---
+++ webpack.prod.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack'); 
+
+module.exports = {
+  entry: [
+    './client/src/index.js'
+  ],
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'bundle.js'
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    }],
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  reload: true
+};
