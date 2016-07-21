f21847d4546574c4e58ce06b3d532c0cca640b4e
Justin Lim <liminator1106@gmail.com>
2016-07-21 10:51:39 -0700

    testing server file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require ('body-parser');
-const app = express();
 const port = process.env.PORT || 8000;
+const app = express();
 
 require('./config/middleware')(app, express);
 require('./routes')(app);
 
-app.listen(process.env.PORT, function() {
+app.listen(port, function() {
   console.log('Listening on 8000...')
 });
+
+app.post('/yelp', (req, res) => {
+
+  res.send('this is a test');
+})
