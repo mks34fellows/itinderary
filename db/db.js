c5933c577114ce4cdd64d7f79cf53304892f0825
Justin Lim <liminator1106@gmail.com>
2016-07-23 17:25:55 -0700

    added db connection and table schema

--- db/db.js
+++ db/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'capleb'
-});
+// require('dotenv').config({silent: true});
 
-// console.log("conn", connection)
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
 
 connection.connect(function(err){
   if(err){
@@ -15,3 +16,20 @@ connection.connect(function(err){
   }
   console.log('Connection established');
 });
+
+connection.query('CREATE TABLE IF NOT EXISTS Users (id INT(11) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30), password VARCHAR(20));', (err) => {
+  if(err) throw err;
+  console.log('Users Table Created');
+});
+
+connection.query('CREATE TABLE IF NOT EXISTS Activities (id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), address VARCHAR(100), phone_number VARCHAR(20), rating VARCHAR(5), image VARCHAR(100));', (err) => {
+  if(err) throw err;
+  console.log('Activities Table Created');
+});
+
+connection.query('CREATE TABLE IF NOT EXISTS Users_Activities (id INT(11) AUTO_INCREMENT PRIMARY KEY, user_id INT(11), activities_id INT(11), FOREIGN KEY (user_id) REFERENCES Users(id), FOREIGN KEY (activities_id) REFERENCES Activities(id));', (err) => {
+  if(err) throw err;
+  console.log('Relational Table Created');
+});
+
+module.exports = connection;
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const yelpSearch = require('./api/yelp');
 const reverseLoc = require('./api/google');
+const db_connection = require('../db/db.js');
 
 module.exports = function (app) {
   // Handles post request from action
@@ -13,10 +14,26 @@ module.exports = function (app) {
 
       // Sends yelpSearch results to client
       yelpSearch(location, term).then((data) => {
+        // data.businesses.forEach((business) => {
+        //   var name = business.name;
+        //   var phone = business.display_phone;
+        //   var rating = buisness.rating;
+        //   var image = buisness.image_url;
+
+
+        //   db_connection.query('SELECT * FROM Activites WHERE phone_number = ?', [phone], (err, rows) => {
+        //     if(err) throw err;
+        //     if(rows.length === 0){
+        //       db_connection.query('INSERT INTO Activites (name, phone_number, rating, image) VALUES (?)', [name, phone, rating, image], (err, info) => {
+        //         if(err) throw err;
+        //         console.log('this worked! WOOHOO!');
+        //       })
+        //     }
+        //   });
+        // })
         
         res.send(data);
       })
     });
-  });
- 
+  }); 
 };
